4b10d26fe3c9feffb8c32b3cf2230dad009e929c
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-02-18 14:03:10 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - print 00 to 99 with no duplicate digits or combos: no 11, no 10 (01)
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int Ones;
+	int Tens;
+
+	for (Tens = '0'; Tens <= '9'; Tens++) /*increment Tens*/
+	{
+		for (Ones = (Tens + 1); Ones <= '9'; Ones++) /*One's Ten+1*/
+		{
+			putchar(Tens);
+			putchar(Ones);
+
+			if (Tens != '8' || ones != '9') /*print commas*/
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
