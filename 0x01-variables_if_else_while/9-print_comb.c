076461f4d8249d94f2b4911e6df9311e88c04fd0
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-02-18 13:51:31 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - print 0-9 separated with commas, using putchar
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p = '0';
+
+	while (p <= '9')
+	{
+		putchar(n);
+		if (p != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		p++;
+	}
+	putchar('\n');
+
+	return (0);
+}
