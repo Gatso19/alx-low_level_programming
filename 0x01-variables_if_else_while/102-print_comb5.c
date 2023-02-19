0273d9ffa7d1d1ab9b95c20610fe8fc28145c2d8
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-02-19 04:23:02 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 	{
 		for (ones = '0'; ones <= '9'; ones++)
 		{
-			for (t = tens; t = '9'; t++) /*print second of pair*/
+			for (t = tens; t <= '9'; t++) /*print second of pair*/
 			{
 				for (o = ones + 1; o <= '9'; o++)
 				{
