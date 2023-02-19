1a9485e9078e9321cdfbefc95c1fef21c8270847
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-02-19 04:04:57 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,33 +7,32 @@
 
 int main(void)
 {
-	int Tens;
-	int Ones;
-	int T;
-	int O;
+	int tens;
+	int ones;
+	int t;
+	int o;
 
-	for (Tens = '0'; Tens <= '9'; Tens++) /*print first two digit combo*/
+	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
 	{
-		for (Ones = '0'; Ones <= '9'; Ones++)
+		for (ones = '0'; ones <= '9'; ones++)
 		{
-		        for (T = Tens; T <= '9'; T++) /*print second of pair*/
+			for (t = tens; t = '9'; t++) /*print second of pair*/
 			{
-				for (O = Ones + 1; O <= '9'; O++)
+				for (o = ones + 1; o <= '9'; o++)
 				{
-					putchar(Tens);
-					putchar(Ones);
+					putchar(tens);
+					putchar(ones);
 					putchar(' ');
-					putchar(T);
-					putchar(O);
+					putchar(t);
 
-					if (!((Tens == '9' && Ones == '8') &&
-					      (T == '9' && O == '9')))
+					if (!((tens == '9' && ones == '8') &&
+					      (t == '9' && o == '9')))
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				O = '0';
+				o = '0';
 			}
 		}
 	}
