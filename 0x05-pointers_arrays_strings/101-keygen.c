14d50e471f924188c02331139f7d0e383346d02b
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-02-28 09:39:56 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,3 +15,31 @@ void generate_password(char *password) {
     }
     password[PASSWORD_LENGTH] = '\0'; /*Add null terminator to the end*/
 }
+
+/*Function to check if a password is valid*/
+int is_valid_password(const char *password) {
+    int i;
+    if (strlen(password) != PASSWORD_LENGTH) {
+        return 0;  /*Password is not the right length*/
+    }
+    for (i = 0; i < PASSWORD_LENGTH; i++) {
+        if (!isalnum(password[i])) {
+            return 0; /*Password contains non-alphanumeric characters*/
+        }
+    }
+    return 1; /*Password is valid*/
+}
+
+/*Main function*/
+int main() {
+    char password[PASSWORD_LENGTH + 1]; /*Add 1 to include null terminator*/
+    srand(time(NULL)); /*Seed the random number generator*/
+    while (1) {
+        generate_password(password);
+        if (is_valid_password(password)) {
+            printf("Generated password: %s\n", password);
+            break;
+        }
+    }
+    return 0;
+}
