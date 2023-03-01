a3d34e84be10d1f224fdf2ea4d83f66a87504413
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-01 10:39:58 -0800

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,26 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-	return (dest);
 
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0'
+	{
+
+	      dest[i] = src[j];
+	      i++;
+	      j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
 }
+
+
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN.H
+#define MAIN.H
+
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
