3c0564fac9f636966b1c8a6514d73c711345d94d
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-01 12:27:20 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * _strncat - concatenate n bytes to destination string
@@ -26,23 +26,3 @@ char *_strncat(char *dest, char *src, int n)
 
 	return (dest);
 }
-/*
- * another method
- * char *_strncat(char *dest, char *src, int n)
-{
-   //while dest exists and isn't null terminator yet
-   while (*dest)
-       dest++;
-   // replace dest while src exists and insert nulls till n bytes
-   for (i = 0; i < n; i++)
-   {
-	while (*src)
-	{
-		*dest = *src;
-		dest++;
-		src++;
-	}
-	*dest = '\0';
-   }
-}
-/*
