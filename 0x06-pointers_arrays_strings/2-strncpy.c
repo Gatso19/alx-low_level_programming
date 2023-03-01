cb99a4c89acaec843e3db8e635f6fd9bfbd29050
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-01 12:32:36 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /*
  * _strncpy - copies n bytes of a source into buffer of a destination string,
@@ -22,18 +22,3 @@ char *_strncpy(char *dest, char *src, int n)
 
 	return (dest);
 }
-/* 
- * this method in pointer arithmetic
- *char *_strncpy(char *dest, char *src, int n)
-{
-  int i;
-  for (i = 0; i < n && *(src + i); i++)
-      *(dest + i) = *(src + i);
-  while (i < n)
-  {
-      *(dest + i) = '\0';
-      i++;
-  }
-  return (dest);
-}
-*/
