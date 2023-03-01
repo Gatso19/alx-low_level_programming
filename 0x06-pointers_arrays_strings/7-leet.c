00f5b9c667beae11d6afa39352d6c70881debcad
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-01 13:11:15 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,5 @@
-#include "main.h"
-
-/**
- * leet - encode string into 1337 leet
+/** 
+* leet - encode string into 1337 leet
  * @s: string to manipulate
  * Return: string
  */
@@ -9,19 +7,19 @@
 char *leet(char *s)
 {
 
-	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+        int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+        int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
-	int i, j;
+        int i, j;
 
-	for (j = 0; s[j] != '\0'; j++)
-	{
-		for (i = 0; a[i] != '\0'; i++)
-		{
-			if (s[j] == a[i])
-				s[j] = b[i];
-		}
-	}
+        for (j = 0; s[j] != '\0'; j++)
+        {
+                for (i = 0; a[i] != '\0'; i++)
+                {
+                        if (s[j] == a[i])
+                                s[j] = b[i];
+                }
+        }
 
-	return (s);
+        return (s);
 }
