13f3d43561eeb59527d991f1f9880210116794ef
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-06 12:26:11 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,22 +8,23 @@
  * Return: pointer to index of string at first occurence
  */
 
-char _*strpbrk (char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int i = 0, j;
 
-	while (s[i] != '\0')
+	while (s[i] != '\0') /*iterate through string*/
 	{
 
-		for (j = 0; accept[j] != '\0'; j++)
+		for (j = 0; accept[j] != '\0'; j++) /* iterate through target */
 		{
-			if (s[i] == accept[j])
+			if (s[i] == accept[j]) /* stop at first match */
 			{
-				s = &s[i];
+				s = &s[i]; /* set pointer to first occurence */
 				return (s);
+			}
 		}
 		i++;
 	}
-	return (NULL);
+	return (NULL); /* return NULL if no matches */
 
-}
+}i
