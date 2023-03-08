277b29a728d924b21c7d605329898c92ebcc4edd
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-08 13:22:44 -0800

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,28 +1,4 @@
 /**
- * substring_match - check if a substring after wildcard matches s1
- * @s1: one string
- * @s2: one string
- * @after_wldcd: placeholder for position right after wildcard
- * Return: 1 if matched, 0 if not
- */
-
-int substring_match(char *s1, char *s2, char *after_wldcd)
-{
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == '\0' && *s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 + 1));
-	if (*s1 == '\0' && *s2 != '\0')
-		return (0);
-	if (*s2 == '*')
-		return (substring_match(s1, s2 + 1, s2 + 1));
-	if (*s1 == *s2)
-		return (substring_match(s1 + 1, s2 + 1, after_wldcd));
-	else
-		return (substring_match(s1 + 1, after_wldcd, after_wldcd));
-}
-
-/**
  * wildcmp - compare if string with wildcard mattches
  * @s1: one string
  * @s2: one string
@@ -31,11 +7,19 @@ int substring_match(char *s1, char *s2, char *after_wldcd)
 
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	else if (*s2 == '*')
-		return (substring_match(s1, (s2 + 1), (s2 + 1)));
-	else
-		return (0);
+	int i = 0, j = 0;
+	while (s1[i] != '\0' && s2[j] != '\0')
+	{
+		if (s1[i] == s2[j])
+			i++; j++;
+		if (s2[j] == "*")
+		{
+			j++;
+			while ((s1[i] != s2[j]) && s1[i] != '\0')
+				i++;
+		}
+		if (s1[i] != s2[j])
+			return (0);
+	}
+	return (1);
+}
