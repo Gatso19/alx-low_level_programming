e2220ee890702f291d6d1618a662064916745aed
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-08 13:06:43 -0800

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-main.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
+
+	r = _sqrt_recursion(0);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1);
+	printf("%d\n", r);
+	r = _sqrt_recursion(1024);
+	printf("%d\n", r);
+	r = _sqrt_recursion(16);
+	printf("%d\n", r);
+	r = _sqrt_recursion(17);
+	printf("%d\n", r);
+	r = _sqrt_recursion(2147483647);
+	printf("%d\n", r);
+	r = _sqrt_recursion(-5);
+	printf("%d\n", r);
+	return (0);
+}
