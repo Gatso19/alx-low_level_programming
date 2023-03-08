198ac659f735d60d2d6244cf623110ea0b5a99fa
Gatso19 <ponagatso.mdhlalose@gmail.com>
2023-03-08 12:49:05 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * _puts_recursion - print string with new line
  * @s: string to print
@@ -7,12 +8,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+if (*s == '\0')
+{
+_putchar('\n');
+return;
 }
+_putchar(*s);
+s++;
+_puts_recursion(s);
+}
+
--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -9,6 +9,7 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
+
 ~  
