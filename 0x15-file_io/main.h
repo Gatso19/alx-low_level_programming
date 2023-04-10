8ebcccc61e37c0d6472b194533df02d13b9bacd2
Gatso19 <ponagatso.mshlalose@gmail.com>
2023-04-10 13:49:26 -0700

    100-elf_header.c

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <elf.h>
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
-int _putchar(char c);
-
+void read_elf_header(int32_t fd, Elf32_Ehdr *elf_header);
+bool is_ELF(Elf32_Ehdr eh);
+void print_os(Elf32_Ehdr eh);
+void print_typ(Elf32_Ehdr eh);
+void print_clas(Elf32_Ehdr eh);
+void print_dta(Elf32_Ehdr eh);
+void print_vrsn(Elf32_Ehdr eh);
+void print_elf_header(Elf32_Ehdr elf_header);
 #endif
